95d56add076bbcc88cd87daf3c1d28075915e9b2
Alexey <so434@web.de>
2017-12-22 10:41:58 +0100

    Korrektur

--- venv/classy1-7-dz.py
+++ venv/classy1-7-dz.py
@@ -12,22 +12,11 @@ class Animals_on_farm:
         print("Максимальный вес, которого достигает {} равен {} кг."
               .format(self.name.lower(), self.weight))
 
-
-class Mammals_birds(Animals_on_farm):
-    def __init__(self, name, weight, age):
-        super().__init__(name, weight, age)
-
     def give(self, milk, animal_type='"млекопитающие"'):
         self.milk = milk
         self.animal_type = animal_type
         print("{} относится к типу {} и даёт в среднем {} литров молока "
               "в месяц.".format(self.name, self.animal_type, self.milk))
-
-    def give_pork(self, pork, animal_type='"млекопитающие"'):
-        self.pork = pork
-        self.animal_type = animal_type
-        print("{} относится к типу {} и даёт в среднем {} кг мяса.".format
-              (self.name, self.animal_type, self.pork))
 
     def give_bird(self, pork, animal_type='"птицы"'):
         self.pork = pork
@@ -36,24 +25,98 @@ class Mammals_birds(Animals_on_farm):
               .format(self.name, self.animal_type, self.pork))
 
 
-animal_1 = Mammals_birds("Корова", 20, 1200)
+class Cows(Animals_on_farm):
+    def __init__(self, name, weight, age, hooves=4):
+        super().__init__(name, weight, age)
+        self.hooves = hooves
+
+    def horns(self, horns=4):
+        self.horns = horns
+        print("{} имеет {} рога и {} копыта."
+              .format(self.name, self.horns, self.hooves))
+
+class Goats(Animals_on_farm):
+    def __init__(self, name, weight, age):
+        super().__init__(name, weight, age)
+
+    def horns(self):
+        self.horns = "90-100 см"
+        print("{} имеет рога длиной {} и копыта."
+              .format(self.name, self.horns))
+
+class Shepps(Animals_on_farm):
+    def __init__(self, name, weight, age):
+        super().__init__(name, weight, age)
+
+    def latain(self, latain='"Ovis aries"'):
+        self.latain = latain
+        print("{} по латыни {}."
+              .format(self.name, self.latain))
+
+class Pigs(Animals_on_farm):
+    def __init__(self, name, weight, age, animal_type='"млекопитающие"'):
+        super().__init__(name, weight, age)
+        self.animal_type = animal_type
+
+    def give_pork(self, pork):
+        self.pork = pork
+        print("{} относится к типу {} и даёт в среднем {} кг мяса.".format
+              (self.name, self.animal_type, self.pork))
+
+class Ducks(Animals_on_farm):
+    def __init__(self, name, weight, age):
+        super().__init__(name, weight, age)
+
+    def wings(self, wings=2):
+        self.wings = wings
+        print("Кроме того, {} имеет {} крыла."
+              .format(self.name.lower(), self.wings))
+
+class Chickens(Animals_on_farm):
+    def __init__(self, name, weight, age):
+        super().__init__(name, weight, age)
+
+    def eggs(self, wings=2, eggs="1-2"):
+        self.wings = wings
+        self.eggs = eggs
+        print("Кроме того, {} имеет {} крыла и нёсёт по {} яйца в день."
+              .format(self.name.lower(), self.wings, self.eggs))
+
+class Geese(Animals_on_farm):
+    def __init__(self, name, weight, age):
+        super().__init__(name, weight, age)
+
+    def paws(self, paws=2, beak=1):
+        self.paws = paws
+        self.beak = beak
+        print("{} отличается {} лапами и {} клювом."
+              .format(self.name, self.paws, self.beak))
+
+
+animal_1 = Cows("Корова", 20, 1200)
 animal_1.info()
 animal_1.give(300)
-animal_2 = Mammals_birds("Коза", 15, 70)
+animal_1.horns()
+animal_2 = Goats("Коза", 15, 70)
 animal_2.info()
 animal_2.give(50)
-animal_3 = Mammals_birds("Овца", 14, 55)
+animal_2.horns()
+animal_3 = Shepps("Овца", 14, 55)
 animal_3.info()
 animal_3.give(35)
-animal_4 = Mammals_birds("Свинья", 13, 300)
+animal_3.latain()
+animal_4 = Pigs("Свинья", 13, 300)
 animal_4.info()
 animal_4.give_pork(150)
-animal_5 = Mammals_birds("Утка", 20, "3,5")
+animal_5 = Ducks("Утка", 20, "3,5")
 animal_5.info()
 animal_5.give_bird("1,5")
-animal_6 = Mammals_birds("Курица", 13, 5)
+animal_5.wings()
+animal_6 = Chickens("Курица", 13, 5)
 animal_6.info()
 animal_6.give_bird("0,5")
-animal_7 = Mammals_birds("Гусь", 20, 12)
+animal_6.eggs()
+animal_7 = Geese("Гусь", 20, 12)
 animal_7.info()
 animal_7.give_bird("4-5")
+animal_7.paws()
