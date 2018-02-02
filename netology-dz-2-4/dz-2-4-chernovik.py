d7d8ed5b41e51bc93b9710a502caa08318138ff2
Alexey Shapovalov <34490457+ascket@users.noreply.github.com>
2018-02-02 23:18:02 +0100

    Update dz-2-4-chernovik.py
    Update

--- netology-dz-2-4/dz-2-4-chernovik.py
+++ netology-dz-2-4/dz-2-4-chernovik.py
@@ -6,22 +6,24 @@ migrations = 'Migrations'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 abs_file_dir = os.path.abspath(current_dir)
 config_path = os.path.join(abs_file_dir, migrations)
+
 file_list = []
 for i in os.listdir(config_path):
-    if ".sql" in i:
+    if i.endswith("sql"):
         file_list.append(i)
 
+def such_func(file, texter):
+    selection = []
+    for t in file:
+        with open(os.path.join(config_path, t)) as ds:
+            data = ds.read()
+            if texter in data:
+                selection.append(t)
+    print(len(selection))
+    return selection
+    #print("Всего файлов {}".format(len(fert)))
+
+
 while True:
-    def opens():
-        fert = []
-        texter = input("Введите: ")
-        for t in file_list:
-            with open(os.path.join(config_path, t)) as ds:
-                data = ds.read()
-                if texter in data:
-                    fert.append(t)
-        print(fert)
-        print("Всего файлов {}".format(len(fert)))
-
-
-    opens()
+    texter = input("Введите искомую фразу: ")
+    print(such_func(file_list, texter))
