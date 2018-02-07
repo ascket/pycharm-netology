efd713d6a6dbac3523fd4848f57fe8dd07abc2e1
Alexey Shapovalov <34490457+ascket@users.noreply.github.com>
2018-02-07 13:33:04 +0100

    Update and rename dz-2-4-chernovik.py to netology-dz-2-4.py
    Update

--- netology-dz-2-4/netology-dz-2-4.py
+++ netology-dz-2-4/netology-dz-2-4.py
@@ -12,18 +12,20 @@ for i in os.listdir(config_path):
     if i.endswith("sql"):
         file_list.append(i)
 
-def such_func(file, texter):
-    selection = []
-    for t in file:
+def such_func(file_list):
+    texter = input("Введите искомую фразу: ")
+    selections = []
+    for t in file_list:
         with open(os.path.join(config_path, t)) as ds:
             data = ds.read()
             if texter in data:
-                selection.append(t)
-    print(len(selection))
-    return selection
-    #print("Всего файлов {}".format(len(fert)))
+                selections.append(t)
+    print('\n'.join(selections))
+    print("Всего файлов: ", len(selections))
+    return selections
 
 
 while True:
-    texter = input("Введите искомую фразу: ")
-    print(such_func(file_list, texter))
+    such_func(file_list)
+    file_list = such_func(file_list)
+    such_func(file_list)
